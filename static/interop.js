ec4300b9bd8778674d33137c063603eb16edb14f
Kris Jenkins <krisajenkins@gmail.com>
2016-07-20 15:00:04 +0100

    Improves the loading sequence.
    We now load the app JS in the document HEAD, and then run it onLoad.

--- static/interop.js
+++ static/interop.js
@@ -1,64 +1,68 @@
 /*global Elm, firebase, firebaseConfig */
 
-(function () {
-    // Start the Elm App.
-    var app = Elm.App.fullscreen();
+addEventListener(
+    'load',
+    function () {
+        // Start the Elm App.
+        var app = Elm.App.fullscreen();
 
-    // Initialize Firebase
-    var firebaseApp = firebase.initializeApp(firebaseConfig);
+        // Initialize Firebase
+        var firebaseApp = firebase.initializeApp(firebaseConfig);
 
-    // Firebase Auth.
-    app.ports.authenticate.subscribe(function () {
-        firebase.auth().signInAnonymously()
-            .catch(app.ports.authError.send);
-    });
-
-
-    // Looks like this doesn't work at all if it fires before the Elm
-    // app has finished initialising. But why hasn't it, by this
-    // stage???
-    setTimeout(function() {
-        firebaseApp.auth()
-            .onAuthStateChanged(app.ports.authStateChanged.send);
-    }, 1);
+        // Firebase Auth.
+        app.ports.authenticate.subscribe(function () {
+            firebase.auth().signInAnonymously()
+                .catch(app.ports.authError.send);
+        });
 
 
-    // Event.
-    var eventsPath = firebase.database().ref('/events');
+        // Looks like this doesn't work at all if it fires before the Elm
+        // app has finished initialising. But why hasn't it, by this
+        // stage???
+        setTimeout(function() {
+            firebaseApp.auth()
+                .onAuthStateChanged(app.ports.authStateChanged.send);
+        }, 1);
 
-    app.ports.eventListen.subscribe(function (eventId) {
-        var eventPath = eventsPath.child(eventId);
 
-        console.log('LISTENING', eventPath.toString());
-        eventPath.on(
-            'value',
-            function(snapshot) {
-                var rawValue = snapshot.val();
-                console.log('HEARD', rawValue);
+        // Event.
+        var eventsPath = firebase.database().ref('/events');
 
-                app.ports.event.send([eventId, JSON.stringify(rawValue)]);
-            },
-            app.ports.eventError.send
-        );
-    });
+        app.ports.eventListen.subscribe(function (eventId) {
+            var eventPath = eventsPath.child(eventId);
 
-    app.ports.eventSilence.subscribe(function (eventId) {
-        var eventPath = eventsPath.child(eventId);
+            console.log('LISTENING', eventPath.toString());
+            eventPath.on(
+                'value',
+                function(snapshot) {
+                    var rawValue = snapshot.val();
+                    console.log('HEARD', rawValue);
 
-        console.log('SILENCING', eventPath.toString());
-        eventPath.off('value');
-    });
+                    app.ports.event.send([eventId, JSON.stringify(rawValue)]);
+                },
+                app.ports.eventError.send
+            );
+        });
 
-    // Voting.
-    app.ports.voteSend.subscribe(function (msg) {
-        var eventId = msg[0],
-            uid = msg[1],
-            vote = msg[2];
-        var eventPath = eventsPath.child(eventId);
-        var votePath = eventPath.child('votes');
-        var userPath = votePath.child(uid);
+        app.ports.eventSilence.subscribe(function (eventId) {
+            var eventPath = eventsPath.child(eventId);
 
-        userPath.set(vote)
-            .catch(app.ports.voteSendError.send);
-    });
-}());
+            console.log('SILENCING', eventPath.toString());
+            eventPath.off('value');
+        });
+
+        // Voting.
+        app.ports.voteSend.subscribe(function (msg) {
+            var eventId = msg[0],
+                uid = msg[1],
+                vote = msg[2];
+            var eventPath = eventsPath.child(eventId);
+            var votePath = eventPath.child('votes');
+            var userPath = votePath.child(uid);
+
+            userPath.set(vote)
+                .catch(app.ports.voteSendError.send);
+        });
+    },
+    false
+);
