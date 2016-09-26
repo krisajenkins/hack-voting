8d4f7418cece83832d3168894a8cb69eb56517d4
Kris Jenkins <krisajenkins@gmail.com>
2016-09-26 18:04:20 +0100

    Updating emacs settings.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,12 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil (eval local-set-key
-               (kbd "M-e")
-               '(lambda ()
-                  (interactive)
-                  (require 'magit)
-                  (let ((compile-command (format "cd %s; elm build"
-                                                 (magit-toplevel))))
-                    (save-buffer)
-                    (compile compile-command))))))
+((nil
+  (eval set (make-local-variable 'compilation-directory)
+        (magit-toplevel))))
