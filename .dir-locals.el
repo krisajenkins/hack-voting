8a2e235c838713cb10b2f9af548e21c6ef3c491e
Kris Jenkins <krisajenkins@gmail.com>
2017-01-23 16:43:57 +0000

    Removing the local emacs setup file.

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil
-  (eval set (make-local-variable 'compilation-directory)
-        (magit-toplevel))))
